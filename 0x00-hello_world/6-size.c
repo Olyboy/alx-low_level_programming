0bc631af35fdeb03a9f8604b93f117820b94d604
Olly <Olyboy>
2023-07-13 19:19:40 -0700

    My first C project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main - Entry Point
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
@@ -13,5 +12,5 @@ int main(void)
         printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
         printf("Size of float: %d byte(s)\n", sizeof(float));
 
-        return(0)
+        return(0);
 }
